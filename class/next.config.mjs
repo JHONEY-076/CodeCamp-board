b210c7f1fc17b955c9a189adea482295e115a188
JHONEY-076 <ccjh2593@gmail.com>
2024-07-25 17:16:50 +0900

    add

---
+++ class/next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack5: true, webpack: (config, options) => { config.cache = false; return config; },
+};
+
+export default nextConfig;
